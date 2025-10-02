b4d1c7485c82619991f9627115561fedac52099b
Sam Sarette <samuelsarette@linux.com>
2025-10-02 16:10:42 -0400

    Update remove-hooks.bat

--- remove-hooks.bat
+++ remove-hooks.bat
@@ -1,1 +1,20 @@
-@bash remove-hooks.sh
+@echo off
+setlocal
+cd %~dp0
+
+:: Copies the hooks to the git folder
+CALL :uninstall pre-commit-prevent-large-files
+CALL :uninstall pre-commit-prevent-directly-committing-main
+CALL :uninstall pre-commit-prevent-nonascii-files
+CALL :uninstall pre-commit
+
+echo Uninstall complete
+
+GOTO :EOF
+
+:uninstall
+IF NOT EXIST "%~dp0\..\.git\hooks\%1" GOTO :EOF
+
+echo Removing "%1"
+del "%~dp0\..\.git\hooks\%1"
+GOTO :EOF
